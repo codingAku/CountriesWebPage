732ee96af5607b301989b2fe880c2695a9953350
codingAku <egoldferb@gmail.com>
2022-03-16 22:39:48 +0300

    no matches title added

--- ww-new/src/app/pages/page-not-found.component.ts
+++ ww-new/src/app/pages/page-not-found.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent implements OnInit {
+  theme!: Observable<string>;
 
   constructor(private router: Router) { }
 
